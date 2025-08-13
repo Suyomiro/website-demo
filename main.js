ece638d345f8f3083b47d9effde8bf675f668ab0
KHALIQ <152598763+Suyomiro@users.noreply.github.com>
2025-08-13 19:02:32 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+
+const titleText = "MAME DIARRA ❤️🎉";
+  const titleElement = document.getElementById("title");
+
+  function showLetters(text, element, callback) {
+    let index = 0;
+    const interval = setInterval(() => {
+      element.innerHTML += text[index];
+      index++;
+      if (index >= text.length) {
+        clearInterval(interval);
+        if (callback) callback();
+      }
+    }, 200); // 0,2 seconde par lettre
+  }
+
+  showLetters(titleText, titleElement);
